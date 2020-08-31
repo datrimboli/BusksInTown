7e01c6373411ff5cc598206b41862d8f2065a6e1
Kenny Yang <kennyyangh@gmail.com>
2020-08-31 10:52:13 -0700

    more changes

--- routes/socialRoutes.js
+++ routes/socialRoutes.js
@@ -6,26 +6,26 @@ router.get('/socials', (req, res) => {
     .then(socials => res.json(socials))
     .catch(err => console.log(err))
 })
-// router.get('/socials/:id', (req, res) => {
-//     Social.findOne({ where: { id: req.params.id }, include: [Artist] })
-//         .then(social => res.json(social))
-//         .catch(err => console.log(err))
-// })
+router.get('/socials/:id', (req, res) => {
+    Social.findOne({ where: { id: req.params.id }, include: [Artist] })
+        .then(social => res.json(social))
+        .catch(err => console.log(err))
+})
 
 router.post('/socials', (req, res) => {
     Social.create(req.body)
         .then(social => res.json(social))
         .catch(err => console.log(err))
 })
-// router.put('/socials/:id', (req, res) => {
-//     Social.update(req.body, { where: { id: req.params.id } })
-//         .then(() => res.sendStatus(200))
-//         .catch(err => console.log(err))
-// })
+router.put('/socials/:id', (req, res) => {
+    Social.update(req.body, { where: { id: req.params.id } })
+        .then(() => res.sendStatus(200))
+        .catch(err => console.log(err))
+})
 
-// router.delete('/socials/:id', (req, res) => {
-//     Social.destroy({ where: { id: req.params.id } })
-//         .then(() => res.sendStatus(200))
-//         .catch(err => console.log(err))
-// })
+router.delete('/socials/:id', (req, res) => {
+    Social.destroy({ where: { id: req.params.id } })
+        .then(() => res.sendStatus(200))
+        .catch(err => console.log(err))
+})
 module.exports = router
