31cb6750aae568d03eda261375084c621827dffc
Devin Castro <dcrocker21@gmail.com>
2020-09-01 14:37:12 -0700

    fixed artist bio

--- public/artist.js
+++ public/artist.js
@@ -14,37 +14,51 @@ axios.get(`/api/artists/${artistId}`)
     document.getElementById('bandPhoto').innerHTML = `
     <img id="bandPhoto" src="${data.image_url}" alt="band photo">
     `
-
-    document.getElementById('socials').innerHTML = `
-    <h4>Socials</h4>
-    <hr>
-    <li><a href="${data.social.facebook}">Facebook</a></li>
-    <li><a href="${data.social.instagram}">Instagram</a></li>
-    <li><a href="${data.social.spotify}">Spotify</a></li>
-    <li><a href="${data.social.soundcloud}">Soundcloud</a></li>
-    <li><a href="${data.social.twitter}">Twitter</a></li>
-    <li><a href="${data.social.youtube}">Youtube</a></li>
-    
+    document.getElementById('bandBio').innerHTML = `
+    <p>${data.bio}</p>
     `
 
+     
 
+    
+    
     // busks
     data.busks.forEach(busk => {
-
+      
       let buskElem = document.createElement('tr')
       // buskElem.className = 'row'
       buskElem.innerHTML = `
-        <th scope="row">${busk.location}</th>
-        <td>Start:${busk.start}-End:${busk.end}</td>
-        <td>${busk.date}</td>
-        <td>${busk.counter} <button class="btn btn-primary">I'm Attending!</button></td>  
+      <th scope="row">${busk.location}</th>
+      <td>Start:${busk.start}-End:${busk.end}</td>
+      <td>${busk.date}</td>
+      <td>${busk.counter} <button class="btn btn-primary">I'm Attending!</button></td>  
       `
       document.getElementById('busks').append(buskElem)
-
+      
     })
-  
     
-   
+
+    let facebook = data.social.facebook || '/'
+    let instagram = data.social.instagram || '/'
+    let spotify = data.social.spotify || '/'
+    let soudcloud = data.social.soudcloud || '/'
+    let twitter = data.social.twitter || '/'
+    let youtube = data.social.youtube || '/'
+
+    
+        document.getElementById('socials').innerHTML = `
+        <h4>Socials</h4>
+        <hr>
+        <li><a href="${facebook}">Facebook</a></li>
+        <li><a href="${instagram}">Instagram</a></li>
+        <li><a href="${spotify}">Spotify</a></li>
+        <li><a href="${soundcloud}">Soundcloud</a></li>
+        <li><a href="${twitter}">Twitter</a></li>
+        <li><a href="${youtube}">Youtube</a></li>
+        
+        `
+    
+    
 
 
 
