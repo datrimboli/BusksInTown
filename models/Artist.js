67913936675951cb1195eaf55d909586cd508972
Eric Park <epark0995@gmail.com>
2020-09-01 13:56:28 -0700

    axed the members table

--- models/Artist.js
+++ models/Artist.js
@@ -11,9 +11,18 @@ Artist.init({
       type: DataTypes.STRING,
       allowNull: false
     },
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     image_url: {   //artists can add image url and we'll be able to render it onto an html page using img tags. For this project we can just use imgur links. 
       type: DataTypes.STRING, 
       allowNull: true //we can set up a default photo if artists do not upload an image 
+    }, 
+    pin: {
+      type: DataTypes.INTEGER(4),
+      allowNull: false,
+      defaultValue: 9000
     }
   }, { sequelize, modelName: 'artists' })
 
