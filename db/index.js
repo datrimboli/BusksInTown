a7c0202b146c989bdaac0badb7a390222e702cee
Mak <makotorasahi@gmail.com>
2020-09-02 14:08:48 -0700

    a

--- db/index.js
+++ db/index.js
@@ -1,3 +1,3 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:K33nnyishere@localhost/busk_db')
+module.exports = new Sequelize(process.env.JAWSDB_URL || 'mysql://root:rootroot@localhost/busk_db')
