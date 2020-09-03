ce0e7d50d34326700adef82549d329a54211c7ad
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-03 17:49:32 -0400

    hi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 // router.use('/api', require('./burgers_controller.js'))
 router.use('/', require('./viewRoutes.js'))
 router.use('/api', require('./artistRoutes.js'))
-router.use('/api', require('./buskRoutes.js'))
+router.use('/api', require('./buskroutes.js'))
 router.use('/api', require('./socialRoutes.js'))
 console.log("hi")
 module.exports = router
