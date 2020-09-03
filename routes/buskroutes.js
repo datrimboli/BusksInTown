9b8f0fb429a44413da4083d3a2a4007f3f58bd13
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-03 18:02:11 -0400

    seed data

--- routes/buskroutes.js
+++ routes/buskroutes.js
@@ -29,5 +29,73 @@ router.delete('/busks/:id', (req, res) => {
         .then(() => res.sendStatus(200))
         .catch(err => console.log(err))
 })
+router.post('/busks/seed', (req, res) => {
+  Busk.bulkCreate([
+    {
+      date: "2020-09-18",
+      location: "123 Main St",
+      start: "12:00:00",
+      end: "14:00:00",
+      artistId: 1
+    },
+
+    {
+      date: "2020-10-04",
+      location: "Target Parking Lot off McKinley",
+      start: "10:00:00",
+      end: "12:30:00",
+      artistId: 1
+    },
+
+    {
+      date: "2020-09-30",
+      location: "9000 Over Blvd",
+      start: "08:00:00",
+      end: "12:00:00",
+      artistId: 2
+    },
+
+    {
+      date: "2020-11-15",
+      location: "Calum's Backyard",
+      start: "15:00:00",
+      end: "17:00:00",
+      artistId: 5
+    },
+
+    {
+      date: "2020-10-31",
+      location: "504 Fake St",
+      start: "14:00:00",
+      end: "17:00:00",
+      artistId: 2
+    },
+
+    {
+      date: "2021-02-17",
+      location: "UCI Parking Structure",
+      start: "20:00:00",
+      end: "23:59:00",
+      artistId: 3
+    },
+
+    {
+      date: "2020-12-15",
+      location: "88 Rising Ave",
+      start: "21:00:00",
+      end: "22:30:00",
+      artistId: 4
+    },
+
+    {
+      date: "2020-12-25",
+      location: "UTC",
+      start: "12:00:00",
+      end: "03:00:00",
+      artistId: 1
+    },
+  ])
+})
+
 
 module.exports = router
--- routes/socialRoutes.js
+++ routes/socialRoutes.js
@@ -28,4 +28,50 @@ router.delete('/socials/:id', (req, res) => {
         .then(() => res.sendStatus(200))
         .catch(err => console.log(err))
 })
+router.post('socials/seed', (req, res) => {
+  Social.bulkCreate([
+    {
+      spotify: "https://open.spotify.com/artist/27Wo1BSPyBtuVmBg7jYkNm?si=C-KN9cWMRj6em7YMVo_5Hw",
+      instagram: "https://www.instagram.com/theunsungmusic/",
+      soundcloud: "https://soundcloud.com/dcrocker21",
+      youtube: "https://www.youtube.com/watch?v=FG6tzKaOlCI",
+      facebook: "https://www.facebook.com/TheUnsungOfficial/",
+      artistId: 1
+    },
+
+    {
+      spotify: "https://open.spotify.com/artist/21JxgGAXZjSjBg9JRgIo9q",
+      instagram: "https://www.instagram.com/offroadminivan/",
+      soundcloud: "https://soundcloud.com/tooth-and-nail-records/off-road-minivan-spiral-gaze",
+      facebook: "https://www.facebook.com/OffRoadMinivan/",
+      artistId: 2
+    },
+
+    {
+      spotify: "https://open.spotify.com/artist/0L8ExT028jH3ddEcZwqJJ5?si=pbccdsEkRMSnBsTy65S9mw",
+      twitter: "https://twitter.com/chilipeppers?lang=en",
+      instagram: "https://www.instagram.com/chilipeppers/?hl=en",
+      soundcloud: "https://soundcloud.com/red-hot-chili-peppers-official",
+      youtube: "https://www.youtube.com/user/RHCPtv",
+      facebook: "https://www.facebook.com/ChiliPeppers/",
+      artistId: 3
+    },
+
+    {
+      spotify: "https://open.spotify.com/artist/2kxP07DLgs4xlWz8YHlvfh",
+      twitter: "https://twitter.com/nikizefanya",
+      instagram: "https://www.instagram.com/nikizefanya/?hl=en",
+      soundcloud: "https://soundcloud.com/niki-sc",
+      youtube: "https://www.youtube.com/channel/UCaAWRu53UW815hGTmxvHFbA",
+      facebook: "https://www.facebook.com/NIKIZefanya/",
+      artistId: 4
+    },
+
+    {
+      twitter: "https://twitter.com/calumragan56",
+      instagram: "https://www.instagram.com/calumragan45/",
+      artistId: 5
+    },
+  ])
+})
 module.exports = router
