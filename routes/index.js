ac16fa5e2a114193be8e7fdbd09231af650c82e1
Dave Trimboli <davidatrimboli@gmail.com>
2020-09-03 17:47:31 -0400

    log test

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ router.use('/', require('./viewRoutes.js'))
 router.use('/api', require('./artistRoutes.js'))
 router.use('/api', require('./buskRoutes.js'))
 router.use('/api', require('./socialRoutes.js'))
-
+console.log("hi")
 module.exports = router
